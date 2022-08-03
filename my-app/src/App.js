5f8af9a814969d226b1988633080f368c83c396b
Shahil Rauf <Shahilzr@gmail.com>
2022-08-03 14:55:07 -0700

    General Messing Around

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Navbar from './Component//Navbar/Navbar';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <p>Hello World</p>
     </div>
   );
---
+++ my-app/src/Component/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+
+function Navbar() {
+  return (
+    <div className='Navbar'>
+        <p className='CompName'>F.A.N.A</p>
+        <ul>
+          <li>"About us"</li>
+          <li>"Press"</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
