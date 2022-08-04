c68cb24ae495668ab43b5d0501f38edcfed3319e
Shahil Rauf <Shahilzr@gmail.com>
2022-08-03 23:21:03 -0700

    Messing around with Map

---
+++ my-app/src/Component/Navbar/Disclaimers.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Disclaimers() {
+  return (
+    <div>Disclaimers</div>
+  )
+}
+
+export default Disclaimers
--- my-app/src/Component/Navbar/Navbar.js
+++ my-app/src/Component/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Navbar.css';
+import Disclaimers from './Disclaimers'
+import Tabs from './Tabs'
 
 
 function Navbar() {
   return (
     <div className='Navbar'>
+        <Disclaimers />
         <p className='CompName'>F.A.N.A</p>
-        <ul>
-          <li>"About us"</li>
-          <li>"Press"</li>
-        </ul>
+        <Tabs />
     </div>
   )
 }
